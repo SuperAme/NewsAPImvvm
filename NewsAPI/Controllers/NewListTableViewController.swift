2e9f866f6db7c17d0265c0c26a1d9eff88810029
Américo MQ <superame@Americos-MacBook-Air.local>
2021-11-30 17:32:10 -0600

    first commit

---
+++ NewsAPI/Controllers/NewListTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewListTableViewController.swift
+//  NewsAPI
+//
+//  Created by Américo MQ on 30/11/21.
+//
+
+import Foundation
+import UIKit
+
+class NewListTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    func setup() {
+        
+        
+    }
+}
